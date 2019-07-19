03166529856eaec77680d6e9eda5f03d8c5d8844
linlin.leo <linlin.leo@bytedance.com>
2019-07-19 09:17:17 +0800

    提交reducer实现

--- src/Reducer.js
+++ src/Reducer.js
@@ -6,6 +6,8 @@ export default (state, action) => {
     switch (action.type) {
         case ActionTypes.POST_REQUEST:
             return {...state, "aaa": "ccc"};
+        case ActionTypes.ADD_REQUEST:
+            return {...state, "requestList": params}
         default:
             return state
     }
